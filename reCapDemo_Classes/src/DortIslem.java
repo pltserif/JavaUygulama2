eddb8fc3f1488ef72814d5a57b5e4ea864743658
Ali Palta <112908290+pltserif@users.noreply.github.com>
2022-09-25 13:23:41 +0300

    Add files via upload

---
+++ reCapDemo_Classes/src/DortIslem.java
@@ -0,0 +1,30 @@
+
+public class DortIslem {
+
+	public int topla(int sayı1, int sayı2) {
+		int toplam = sayı1 + sayı2;
+
+		return toplam;
+
+	}
+
+	public int çıkar(int sayı1, int sayı2) {
+		int çıkar = sayı1 - sayı2;
+
+		return çıkar;
+
+	}
+	public int böl(int sayı1, int sayı2) {
+		int böl = sayı1 / sayı2;
+
+		return böl;
+
+	}
+	public int çarp(int sayı1, int sayı2) {
+		int çarp = sayı1 * sayı2;
+
+		return çarp;
+
+	}
+
+}
---
+++ reCapDemo_Classes/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.böl(10, 5);
+		System.out.println(sonuc);
+	}
+
+}
